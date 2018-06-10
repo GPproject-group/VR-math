6b0d343fdcc04b17f2bf448cb72e95b9963ceec3
sjtuXZT <787590763@sjtu.edu.cn>
2018-06-10 17:32:16 +0800

    select vertex

---
+++ Assets/Scripts/ChangeMaterialScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterialScript : MonoBehaviour {
+    public Material selectedMaterial;
+    public Material defaultMaterial;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.tag.Equals("selected"))
+        {
+            this.GetComponent<Renderer>().material = selectedMaterial;
+        }
+        if (this.tag.Equals("model"))
+        {
+            this.GetComponent<Renderer>().material = defaultMaterial;
+        }
+    }
+}
---
+++ Assets/Scripts/SelectObjectScript.cs
@@ -0,0 +1,61 @@
+﻿namespace VRTK.Examples
+{
+    using System.Collections.Generic;
+    using UnityEditor;
+    using UnityEngine;
+
+    public class SelectObjectScript : VRTK_InteractableObject
+    {
+        public GameObject controllerRight;
+        private float accuTime;
+        private bool flag;
+
+
+        public override void StartUsing(VRTK_InteractUse usingObject)
+        {
+            base.StartUsing(usingObject);
+            flag = true;
+            if (this.tag.Equals("model"))
+            {
+                accuTime = 0;
+            }
+            if (this.tag.Equals("selected"))
+            {
+                accuTime = 0;
+            }
+        }
+        public override void StopUsing(VRTK_InteractUse usingObject)
+        {
+            base.StopUsing(usingObject);
+        }
+
+        protected void Start()
+        {
+            accuTime = 0;
+            flag = true;
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+            if (this.tag.Equals("model")&&flag)
+            {
+                accuTime += Time.deltaTime;
+                if (accuTime >= 2)
+                {
+                    this.tag = "selected";
+                    flag = false;
+                }
+            }
+            else if (this.tag.Equals("selected")&&flag)
+            {
+                accuTime += Time.deltaTime;
+                if (accuTime >= 2)
+                {
+                    this.tag = "model";
+                    flag = false;
+                }
+            }
+        }
+    }
+}
